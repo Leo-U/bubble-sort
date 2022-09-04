76e1365494f7565a1d11a38449956260a9de7546
Joel Howard <joelhoward2@gmail.com>
2022-09-04 15:38:21 -0400

    refactor 1, edit readme

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,14 +1,11 @@
 arr = [4,3,78,2,0,2]
 
 def bubble_sort (arr)
-  def switch(arr)
-    arr.drop(2).unshift(arr[1],arr[0]) 
-  end
   push_me = []
   loop do
     check_me = arr.dup
     while arr.length > 0 do
-      arr = switch(arr) if arr[1] != nil && arr[0] > arr [1] 
+      arr = arr.drop(2).unshift(arr[1], arr[0]) if arr[1] != nil && arr[0] > arr [1] 
       push_me.push(arr.shift)
     end
     arr = push_me
